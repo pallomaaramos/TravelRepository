481ef6dddfad3ffbd538db564bfed06de26c8ccf
pallomaaramos <pallomaramos.ma@gmail.com>
2020-04-06 16:37:28 -0300

    adiciona

--- MyPlayground.playground/Pages/First.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/First.xcplaygroundpage/Contents.swift
@@ -2,45 +2,115 @@ import UIKit
 import PlaygroundSupport
 
 
-
 class MyViewController: UIViewController {
     
-    let button = UIButton()
+    let play = UIButton()
     
     override func loadView() {
         
         let view = UIView()
         view.backgroundColor = .white
-        let label = UILabel()
-        label.frame = CGRect(x: 30, y: 200, width: 200, height: 20)
-        label.text = "Categorias"
-        label.numberOfLines = 100
-        label.textColor = .black
         
-        button.frame = CGRect(x: 150, y: 220, width: 200, height: 40)
+        let image2 = UIImage(named: "play.png") as UIImage?
+        play.frame =  CGRect(x: 512, y: 303, width: 345, height: 345)
+        play.setImage(image2, for: .normal)
         
-        button.setTitle("Toque", for: .normal)
-        button.setTitleColor(.black, for: .normal)
+        let image = UIImage(named:"fundo.png")
+        let imageView = UIImageView(image: image)
+        imageView.frame = CGRect(x: 0, y: 0, width: 1440, height: 900)
         
-        view.addSubview(label)
-        view.addSubview(button)
-        
-        //        let image = #imageLiteral(resourceName: "dukesamoieda.png")
-        //        let imageView = UIImageView(image: image)
-        //        imageView.frame = CGRect(x: 10, y: 5, width: 475, height: 595)
-        
-        //view.addSubview(imageView)
         self.view = view
+        
+        view.addSubview(imageView)
+        view.addSubview(play)
     }
     
     override func viewDidLoad() {
-        button.addTarget(self, action: #selector(MyViewController.touchedButton), for: .touchUpInside)
+        play.addTarget(self, action: #selector(MyViewController.touchedButton), for: .touchUpInside)
     }
     
     @IBAction func touchedButton() {
-        print("oi")
+        //        print("iniciou")
+        navigationController?.pushViewController(secondViewController, animated: true)
     }
 }
-let vc = MyViewController(screenType: .ipad, isPortrait: false)
-PlaygroundPage.current.liveView = vc.scale(to: 0.4)
+
+class SecondViewController: UIViewController {
+    
+    let clima1 = UIButton()
+    let clima2 = UIButton()
+//    let prox = UIButton()
+    
+    override func loadView() {
+        
+        let view = UIView()
+        
+        let image = UIImage(named:"clima.png")
+        let imageView = UIImageView(image: image)
+        imageView.frame.size = CGSize(width: 1440, height: 900)
+        
+        let image2 = UIImage(named: "climab1.png") as UIImage?
+        clima1.frame =  CGRect(x: 250, y: 396, width: 338, height: 350)
+        clima1.setImage(image2, for: .normal)
+        
+        let image3 = UIImage(named: "climab2.png") as UIImage?
+        clima2.frame =  CGRect(x: 800, y: 396, width: 338, height: 350)
+        clima2.setImage(image3, for: .normal)
+        //
+        
+        //        let image4 = UIImage(named: "next.png")
+        //        prox.frame = CGRect(x: 1140, y: 105, width: 154, height: 154)
+        //        prox.setImage(image4, for: .normal)
+        //
+        let proximo = UIImage(named:"next.png")
+        let proximoView = UIImageView(image: proximo)
+        proximoView.frame = CGRect(x:1140,y:105,width: 154, height: 154)
+        
+        self.view = view
+        view.addSubview(imageView)
+        view.addSubview(clima1)
+        view.addSubview(clima2)
+        view.addSubview(proximoView)
+        //        view.addSubview(prox)
+        
+        proximoView.isUserInteractionEnabled = true
+
+        let tapProx = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+
+        proximoView.addGestureRecognizer(tapProx)
+
+    }
+    @objc func handleTap(_ gesture: UIGestureRecognizer) {
+        navigationController?.pushViewController(thirdViewController, animated: true)
+    }
+    
+    //    override func viewDidLoad() {
+    //        prox.addTarget(self, action: #selector(SecondViewController.touchedButton), for: .touchUpInside)
+    //    }
+    //
+    //    @IBAction func touchedButton() {
+    //        print("iniciou")
+    //        navigationController?.pushViewController(thirdViewController, animated: true)
+    //    }
+}
+
+class ThirdViewController: UIViewController{
+    
+    override func loadView() {
+        let view = UIView()
+        view.frame.size = CGSize(width: 1440, height:900)
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        self.view = view
+    }
+}
+let firstViewController = MyViewController()
+let secondViewController = SecondViewController()
+let thirdViewController = ThirdViewController()
+
+let navigation = UINavigationController(screenType: .mac, isPortrait: true)
+navigation.navigationBar.isHidden = true
+navigation.pushViewController(firstViewController, animated: true)
+
+PlaygroundPage.current.liveView = navigation.scale(to: 0.4)
 
--- MyPlayground.playground/Pages/Second.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Second.xcplaygroundpage/Contents.swift
@@ -4,7 +4,8 @@ import PlaygroundSupport
 
 class ColorViewController: UIViewController {
     var color: UIColor = #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1)
-    
+//    var background: UIImage = UIImageView(image:#imageLiteral(resourceName: <#T##String#>))
+//
     override func loadView() {
         let view = UIView()
         view.backgroundColor = self.color
@@ -14,11 +15,15 @@ class ColorViewController: UIViewController {
 
     }
 
+//let fundo = #imageLiteral(resourceName: "Group 1-2.png")
+
 let categorias = ColorViewController()
 categorias.title = "Categorias"
 
 let destinos = ColorViewController()
-destinos.color = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+//destinos.view = UIImageView (image: fundo)
+//destinos.background = #imageLiteral(resourceName: <#T##String#>)
+destinos.color = .blue
 destinos.title = "Destinos"
 
 
